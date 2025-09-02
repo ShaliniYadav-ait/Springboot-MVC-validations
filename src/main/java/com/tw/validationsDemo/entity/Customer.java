200fa57c6e5159f380fe3c698b746ca9c25090ef
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-02 08:41:46 +0530

    add getter and setters for entity

--- src/main/java/com/tw/validationsDemo/entity/Customer.java
+++ src/main/java/com/tw/validationsDemo/entity/Customer.java
@@ -10,4 +10,24 @@ public class Customer {
     private String firstName;
 
     private String lastName;
+
+    public Customer(){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
