ad79ecbaa51b35adb7446fe16f062446229e5379
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-02 08:32:09 +0530

    Initial setup

---
+++ src/main/java/com/tw/validationsDemo/ValidationsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tw.validationsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tw/validationsDemo/ValidationsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.validationsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
