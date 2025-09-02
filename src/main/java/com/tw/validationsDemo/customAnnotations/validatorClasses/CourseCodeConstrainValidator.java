3a17e5c4b184f4edc914e357be12fe3c1bdde731
shaliniyadav <shaliniyadav.ait@gmail.com>
2025-09-02 10:33:31 +0530

    add custom annotation validation class

---
+++ src/main/java/com/tw/validationsDemo/customAnnotations/validatorClasses/CourseCodeConstrainValidator.java
@@ -0,0 +1,23 @@
+package com.tw.validationsDemo.customAnnotations.validatorClasses;
+
+import com.tw.validationsDemo.customAnnotations.CourseCode;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstrainValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+
+    @Override
+    public void initialize(CourseCode theCourseCode) {
+        coursePrefix = theCourseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
+
+        boolean result = theCode.startsWith(coursePrefix);
+        return result;
+    }
+}
